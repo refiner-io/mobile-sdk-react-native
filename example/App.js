9693a7e91c7fb231dd23d246d54bb84b7bbdb7e8
Alper Tekin <alpertekin6@gmail.com>
2022-04-29 00:47:24 +0300

    Change example project

---
+++ example/App.js
@@ -0,0 +1,120 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import RNRefiner from 'refiner-react-native';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  var userTraits = { email: "hello@hello.com", a_number: 123, a_date: "2022-16-04 12:00:00" };
+
+  RNRefiner.initialize("56421950-5d32-11ea-9bb4-9f1f1a987a49");
+  RNRefiner.identifyUser("my-user-id", null, null);
+  RNRefiner.showForm("616fc500-5d32-11ea-8fd5-f140dbcb9780", true);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
--- ios/RNRefiner.m
+++ ios/RNRefiner.m
@@ -11,39 +11,39 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(initialize:(RefinerConfigs *)refinerConfigs)
+RCT_EXPORT_METHOD(initialize:(NSString *)projectId)
 {
-    [[Refiner shared] initializeWithValue:refinerConfigs];
+    [[Refiner shared] initialize:RefinerConfigs(projectId)];
 }
 
-RCT_EXPORT_METHOD(identifyUser:(NSString *)userId)
+RCT_EXPORT_METHOD(identifyUser:(NSString *)userId (NSDictionary *) userTraits (NSString *)locale)
 {
-    [[Refiner shared] identifyUserWithValue:userId];
+    [[Refiner shared] identifyUser:userId userTraits:userTraits locale:locale];
 }
 
-RCT_EXPORT_METHOD(resetUser:(NSString *)eventName)
+RCT_EXPORT_METHOD(resetUser)
 {
     [[Refiner shared] resetUser];
 }
 
 RCT_EXPORT_METHOD(trackEvent:(NSString *)eventName)
 {
-    [[Refiner shared] trackEventWithValue:eventName];
+    [[Refiner shared] trackEvent:eventName];
 }
 
 RCT_EXPORT_METHOD(trackScreen:(NSString *)screenName)
 {
-    [[Refiner shared] trackScreenWithValue:screenName];
+    [[Refiner shared] trackScreen:screenName];
 }
 
 RCT_EXPORT_METHOD(showForm:(NSString *)formUuid (BOOL *)force)
 {
-    [[Refiner shared] showFormWithValue:(formUuid, force)];
+    [[Refiner shared] showForm:formUuid force];
 }
 
 RCT_EXPORT_METHOD(attachToResponse:(NSDictionary *)contextualData)
 {
-    [[Refiner shared] attachToResponseWithValue:contextualData];
+    [[Refiner shared] attachToResponse:contextualData];
 }
 
 @end
